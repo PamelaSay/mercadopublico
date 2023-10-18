eee836f3d3d2260ddfa666d82ba85d8ab94c2970
PamelaSay <147214129+PamelaSay@users.noreply.github.com>
2023-10-18 03:23:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -21,6 +21,29 @@
   document.getElementById('Quizz').addEventListener('click',abrirQuizz);
 
 
+
+        function calcularPontuacao(){
+            //Inicializa a pontuação
+            var pontuacao = 0;
+
+            //Obtém as respostas selecionadas
+            var respostas =
+document.forms["quizzForm"].elements;
+
+            //Loop pelas respostas e soma os valores de pontuação das respostas corretas
+            for(var i=0; i<respostas.length; i++) {
+                                            if(respostas[i].type == 'radio' && respostas[i].checked){
+                                                                             pontuacao += parseIn(respostas[i].value);
+                                            }
+            }
+            //Exibe a pontuação
+            var resultadoDiv =
+document.getElementById("resultado");
+            resultadoDiv.innerHTML = "Sua pontuação é:" + pontuacao;
+        }
+   
+
+
 //exemplos
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Olá mundo!";
