581eea1bd3ebfa4fa97ae8c4e7b11a1494877dc2
PamelaSay <147214129+PamelaSay@users.noreply.github.com>
2023-10-21 22:26:51 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+     question: "Quanto é 1+2?";
+        answers:[
+            {Option:"3", correct: true } ;
+            {Option:"2", correct: false };
+            {Option:"4", correct: false };
+        ];
+    };
+    {
+     question: "Quanto é 5x2?";
+        answers:[
+            {Option:"9", correct: false } ;
+            {Option:"10", correct: true };
+            {Option:"11", correct: false };
+        ];
+    };
+    {
+     question: "Quanto é 10x8?";
+        answers:[
+            {Option:"800", correct: false } ;
+            {Option:"90", correct: false };
+            {Option:"80", correct: true };
+        ];
+    };
+    {
+     question: "Quanto é 7x7?";
+        answers:[
+            {Option:"14", correct: false };
+            {Option:"4", correct: false };
+            {Option:"49", correct: true };
+        ];
+    };
+]
