91cfd79832b77591615df2c1d4d74fc64e0563df
PamelaSay <147214129+PamelaSay@users.noreply.github.com>
2023-11-09 01:54:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,58 +11,54 @@ import question from "questions.js";
 let currentIndex = 0;
 let questionsCorrect = 0;
 
-btnRestart.onclick=()=>{
-    content.style.display="flex";
-    contentFinish.style.display="none";
+btnRestart.onclick = () => {
+  content.style.display = "flex";
+  contentFinish.style.display = "none";
 
-    currentIndex = 0;
-    questionsCorrect = 0;
-    loadQuestion();
+  currentIndex = 0;
+  questionsCorrect = 0;
+  loadQuestion();
 };
 
-function nestQuestion(e){
-    if(e.target.getAttribute("data-correct") === "true"){
-        questionsCorrect++;
-    }
-    if (currentIndex < questions.length -1){
-        currentIndex++;
-        loadQuestion();
-    }else{
-        finish ();
-    }
-    }
+function nextQuestion(e) {
+  if (e.target.getAttribute("data-correct") === "true") {
+    questionsCorrect++;
+  }
 
-    function finish(){
-        textFinish.innerHTML = "você acertou ${questionsCorrect"} de ${questions.length}";
-        content.style.display="none";
-        contentFinish.style.display = "flex";
-    }
+  if (currentIndex < questions.length - 1) {
+    currentIndex++;
+    loadQuestion();
+  } else {
+    finish();
+  }
+}
+function finish() {
+  textFinish.innerHTML = `você acertou ${questionsCorrect} de ${questions.length}`;
+  content.style.display = "none";
+  contentFinish.style.display = "flex";
 }
 
+function loadQuestion() {
+  spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
+  const item = questions[currentIndex];
+  answers.innerHTML = "";
+  question.innerHTML = item.question;
 
-loadQuestion();
-function loadQuestion(){
-    spnQtd.innerHTML= "${currentIndex+1}/${questions.length}";
-    const item =questions[currentIndex];
-    answers.innerHTML = "";
-    question.innerHTML = item.question;
+  item.answers.forEach((answer) => {
+    const div = document.createElement("div");
 
-    item.answers.array.forEach((answers) => {
-     const div = document.createElement("div");
+    div.innerHTML = `
+    <button class="answer" data-correct="${answer.correct}">
+      ${answer.option}
+    </button>
+    `;
 
-     div.innerHTML=
-     <button class="answer" data-correct="${answer.correct}">
-         ${answers.option} 
-     </button>
-     ;
-     answers.appendChild(div);
-    });
-    document.querySelectorAll(".answer").forEach((item)=>{
-        item.addEventListener("click", nestQuestion);
-    });
+    answers.appendChild(div);
+  });
+
+  document.querySelectorAll(".answer").forEach((item) => {
+    item.addEventListener("click", nextQuestion);
+  });
 }
 
-
-    
 loadQuestion();
-
