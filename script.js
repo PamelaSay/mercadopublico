dec955c24e206e6f441761449f0b73f363d1e007
PamelaSay <147214129+PamelaSay@users.noreply.github.com>
2023-11-09 01:13:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const question = document.querySelector(".question");
-const answers = document.querySelector(".answers");
-const spnQtd = document.querySelector(".spnQtd");
-const textFinish = document.querySelector(".finish span");
-const content = document.querySelector(".content");
-const contentFinish = document.querySelector(".finish");
-const btnRestart = document.querySelector(".finish button");
+const question = document.querySelector("question");
+const answers = document.querySelector("answers");
+const spnQtd = document.querySelector("spnQtd");
+const textFinish = document.querySelector("finish span");
+const content = document.querySelector("content");
+const contentFinish = document.querySelector("finish");
+const btnRestart = document.querySelector("finish button");
 
-import question from "./questions";
+import question from "questions.js";
 
 let currentIndex = 0;
 let questionsCorrect = 0;
