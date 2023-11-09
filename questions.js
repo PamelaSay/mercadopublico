43bf03a94e24d42d586f2a2e960485b4fd97940d
PamelaSay <147214129+PamelaSay@users.noreply.github.com>
2023-11-09 01:35:25 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,34 +1,34 @@
 export default [
-    {
-     question:"Quanto é 1+2?",
-     answers:[
-        {option:"3", correct:true},
-        {option:"2", correct:false},
-        {option:"4", correct:false},
-    ],
-    },
-    {         
-     question:"Quanto é 5x2?",
-     answers:[
-        {option:"9", correct:false},
-        {option:"10", correct:true},
-        {option:"11", correct:false},
-    ],
-    },
+ {
+  question:"Quanto é 1+2?";
+   answers:[
+   {option:"3", correct:true};
+   {option:"2", correct:false};
+   {option:"4", correct:false};
+    ];
+ };
+ {         
+   question:"Quanto é 5x2?";
+   answers:[
+    {option:"9", correct:false};
+    {option:"10", correct:true};
+    {option:"11", correct:false};
+    ];
+ };
     {   
-     question:"Quanto é 10*8?",
+     question:"Quanto é 10*8?";
      answers:[
-        {option:"800", correct:true},
-        {option:"90", correct:false},
-        {option:"80", correct:true},
-    ],
-    }
-    {
-     question:"Quanto é 1+2?",
+        {option:"800", correct:true};
+        {option:"90", correct:false};
+        {option:"80", correct:true};
+      ];
+  };
+  {
+     question:"Quanto é 1+2?";
      answers:[
-         {option:"3", correct:true},
-        {option:"2", correct:false},
-        {option:"4", correct:false},
-    ],
-    }
-]
+        {option:"3", correct:true};
+        {option:"2", correct:false};
+        {option:"4", correct:false};
+    ];
+  };
+];
