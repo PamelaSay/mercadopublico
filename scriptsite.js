a2569cc3d9e85ac0bea7afea89886b07685f83a1
PamelaSay <147214129+PamelaSay@users.noreply.github.com>
2023-11-10 11:11:25 -0300

    Update scriptsite.js

--- scriptsite.js
+++ scriptsite.js
@@ -1,3 +1,15 @@
+const btnMobile=document.getElementById("btn_mobile");
+
+function toggleMenu(){
+  const navbar=document.getElementById("navbar");
+  nav.classList.toggleMenu("active");
+}
+
+btnMobile.addEventListener("click, togglerMenu")
+
+
+
+
 function menuShow(){
   let menumobile = document.querySelector(".mobile_menu");
   if(menumobile.classList.contains("open")){
