83f7f33871043bff7b5de474b6425ad030d6d2b9
PamelaSay <147214129+PamelaSay@users.noreply.github.com>
2023-11-10 17:46:44 -0300

    Update scriptsite.js

--- scriptsite.js
+++ scriptsite.js
@@ -1,10 +1,10 @@
 
 function menuShow(){
-  let menuMobile= document.querySelector('.mobile_menu');
-  if(menuMobile.classList.contaisn('open')){}
-    menuMobile.classList.remove('open');
+  let menuMobile= document.querySelector(".mobile_menu");
+  if(menuMobile.classList.contaisn("open")){}
+    menuMobile.classList.remove(".open");
 }else{
-  menuMobile.classList.add('open');
+  menuMobile.classList.add("open");
 }
 
 
