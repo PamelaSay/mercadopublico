955121887121f23b984083aa9045307d71f0eb1b
PamelaSay <147214129+PamelaSay@users.noreply.github.com>
2023-11-10 18:52:46 -0300

    Update scriptsite.js

--- scriptsite.js
+++ scriptsite.js
@@ -1,12 +1,12 @@
 
 function menuShow(){
-  let menuMobile= document.querySelector('.mobile_menu');
-  if(menuMobile.classList.contaisn('open')){}
-    menuMobile.classList.remove('open');
-    document.querySelector('icon').src = "iconemenu2.png'";
+  let menuMobile= document.querySelector(".mobile_menu");
+  if(menuMobile.classList.contaisn("open")){
+    menuMobile.classList.remove("open");
+    document.querySelector("icon").src = "iconemenu2.png'";
   }else{
-    menuMobile.classList.add('open');
-    document.querySelector('icon').src = "iconemenu2.png'";
+    menuMobile.classList.add("open");
+    document.querySelector("icon").src = "iconemenu2.png'";
 }
 }
 
